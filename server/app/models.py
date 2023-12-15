119efba8a28b88a2d50daa7902ffaf1b7c35f5c2
alexxqq <132622405+alexxqq@users.noreply.github.com>
2023-12-15 18:16:42 +0200

    Delete server directory

--- server/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class NodeModel(models.Model):
-    name = models.CharField(max_length = 255)
-    messages = models.IntegerField()
-    date_created = models.DateTimeField()
-    is_active = models.BooleanField(default = False)
